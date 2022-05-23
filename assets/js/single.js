7bff7ef26382fdee8d07bfdf13880efa8063ccc7
noothanks <coreyfuller727@yahoo.com>
2022-05-23 15:37:33 -0400

    Adding link to github for repos with more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,18 @@
 var issuesContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,9 +21,14 @@ var getRepoIssues = function(repo) {
         //request successful
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+
                 //pass data to be added to DOM
                 displayIssues(data);
+
+                 // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                console.log(displayWarning(repo));
+                    }
             });
         } else {
             alert("There was a problem with your request!");
